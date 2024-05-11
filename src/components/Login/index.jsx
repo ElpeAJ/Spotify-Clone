6a0bd90105dfda1c630b6b08aed991821c0d5e14
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-05-11 11:16:00 -0700

    Made Changes

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -110,7 +110,7 @@ function Login() {
       <span></span>
       <div content="" className={styles.form}>
         <Button className={styles.button}>
-          <img src={Facebook} alt="facebook image" />
+          <img src={Facebook} alt="facebook" />
           <p className={styles.btnText}>Continue with Facebook</p>
         </Button>
         <Button className={styles.button}>
